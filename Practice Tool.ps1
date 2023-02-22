eaeb55a542649ab9cc9e0b8c1dd6324cedd3dc54
AngryGroceries <spammy246@gmail.com>
2023-02-22 15:33:38 -0700

    Update Practice Tool.ps1
    Fixed some formatting
    Added additional output formatting
    Made it so the only "hard" barrier is the initial "Jump" key
    It was easy to accidentally flood the screen with outputs, so added some logic to reduce this.
    
    By clearing the second barrier that required "crouch" it was easy to accidentally be caught in a backwards jump/crouch loop. Added a 125 millisecond delay input buffer clear to stop that from happening

--- Practice Tool.ps1
+++ Practice Tool.ps1
@@ -2,12 +2,13 @@
 # JayTheYggdrasil @ https://github.com/JayTheYggdrasil
 # AngryGroceries  @ https://github.com/AngryGroceries 
 
-
 $loop = "true"
 $currenttime = Get-Date
 $secondtime = Get-Date
 $presscount = 0
 $evenodd = $presscount % 2
+$jumpfirst = 0
+$removespam = 0
 
 "!! Use CTRL+C to quit !!" 
 "-- Setup --"
@@ -18,9 +19,9 @@ $jumpkey = $Host.UI.RawUI.ReadKey()
 $duckkey = $Host.UI.RawUI.ReadKey()
 ""
 
-write-host -nonewline "Enter your framrate: "
-  $inputString = read-host
-  $targetfps = $inputString -as [Double]
+write-host -nonewline "Enter your framerate: "
+$inputString = read-host
+$targetfps = $inputString -as [Double]
 
 write-host -nonewline "Jump + crouch must be exactly 1 frame apart for the highest chance at superglide success."
 
@@ -34,7 +35,10 @@ $frametime = 1 / $targetfps
 while ($loop -eq "true") {
 
    if ($evenodd -eq 0) {
-      "Press Jump..."
+      if ($removespam -eq 0){
+	      "Press Jump..."
+      }
+
       $key = $Host.UI.RawUI.ReadKey()
       
       if($key -eq $jumpkey) {
@@ -43,12 +47,23 @@ while ($loop -eq "true") {
          " (Jump) Key Pressed"
          $presscount = $presscount + 1
          $evenodd = $presscount % 2
-      } else {
-         " that's not jump..."
-      }
+         $jumpfirst = 1
+      }  
+         elseif ($key -eq $duckkey) {
+             $currenttime = Get-Date
+
+            " (Crouch) Key Pressed"
+            $presscount = $presscount + 1
+            $evenodd = $presscount % 2
+      }  
+         else {
+	         " Pressed. Did not hit the (Jump) key."
+	         $removespam = 1
+         }
    }
 
    if ($evenodd -eq 1) {
+      $removespam = 0
       "Press Crouch..."
       $key = $Host.UI.RawUI.ReadKey()
 
@@ -63,13 +78,15 @@ while ($loop -eq "true") {
          if($elapsedFrames -lt 1) {
             $chance = $elapsedFrames * 100
             $message = "Crouch slightly *later* by {0:n5} seconds" -f $differenceSeconds + " to improve."
-         } elseif ($elapsedFrames -lt 2) {
+         }  
+            elseif ($elapsedFrames -lt 2) {
             $chance = ( (2 - $elapsedFrames) ) * 100
             $message = "Crouch slightly *sooner* by {0:n5} seconds" -f ($differenceSeconds * -1) + " to improve."
-         } else {
-            $message = "Crouched too late by " + ($elapsedFrames - 1).ToString("###") + " frames."
+            } 
+            else {
+            $message = "Crouched too late by {0:n2} frames." -f ($elapsedFrames - 1)
             $chance = 0
-         }
+            }
 
          ("{0:n3} frames have passed." -f $elapsedFrames.ToString()) | Write-Host
          
@@ -80,8 +97,40 @@ while ($loop -eq "true") {
          $presscount = $presscount + 1
          $evenodd = $presscount % 2
          "--------------------------------------------------"
-      } else {
-         " that's not crouch..."
-      }
+      }  
+         else {
+		      $secondtime = Get-Date
+         	$calculated = $secondtime - $currenttime
+         	$elapsedFrames = $calculated.TotalSeconds / $frametime
+	 	      $differenceSeconds = $frametime + $calculated.TotalSeconds
+		      $chance = 0
+		      $message = "Oops! Wrong key order by {0:n5} seconds or {1:n3} frames. " -f $calculated.TotalSeconds, $elapsedFrames
+
+		
+		      if($key -eq $jumpkey -And $jumpfirst -eq 1){
+			      " (Jump) Key double-tapped."
+				}
+		         elseif($key -eq $duckkey) {
+			      " (Crouch) Key double-tapped."
+				} 	
+		         elseif($key -eq $jumpkey) {
+			      " (Jump) Key Pressed."
+				} 		
+		         else{
+			      " Key Pressed." | Write-Host
+		      }		
+		 
+	         ( "{0:n4} % chance to hit." -f $chance.ToString() ) | Write-Host
+	         $message | Write-Host
+
+            $presscount = $presscount + 1
+            $evenodd = $presscount % 2
+	         "--------------------------------------------------"
+
+
+         }
    }
+   $jumpfirst = 0
+	Start-Sleep -Milliseconds 125
+	$HOST.UI.RawUI.Flushinputbuffer()
 }
