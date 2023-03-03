3c4d82785c52ba76cd4cfd879614bfae4f2d8e5c
JayTheYggdrasil <thatotherrandomguy@gmail.com>
2023-03-03 02:36:22 -0500

    Fixing Errors

--- Practice Tool.ps1
+++ Practice Tool.ps1
@@ -7,13 +7,14 @@ $startTime = Get-Date
 $secondtime = Get-Date
 
 enum states {
-   Ready,      # Initial State
-   Jump,       # Partial Sequence
-   JumpWarned, # Multi-Jump Warning Sent
-   Crouch,     # Incorrect Sequence, let it play out for a bit
+   Ready      # Initial State
+   Jump       # Partial Sequence
+   JumpWarned # Multi-Jump Warning Sent
+   Crouch     # Incorrect Sequence, let it play out for a bit
 }
 
 $state = [states]::Ready
+$lastState = [states]::Jump
 
 "!! Use CTRL+C to quit !!" 
 "-- Setup --"
@@ -39,107 +40,119 @@ $frameTime = 1 / $targetfps
 $attempt = 0
 $cumlative = 0
 $chance = 0
-"###### Attempt 0 - Average: NA ######"
-"Awaiting Jump..."
-
-Function ReadyUp() {
-   $attempt = $attempt + 1
-   $cumlative = $cumlative + $chance
-
-   $average = $cumlative - $attempt
-   "###### Attempt {0:n4} - Average: {0:n2} ######" -f $attempt, $average
-   Write-Host -ForegroundColor Gray "Awaiting Jump..."
-   $state = [states]::Ready
-}
 
 while ($loop -eq "true") {
-      # Get keys
-
+   
+   # Status Update(s) - only on state change
+   if(!($lastState -eq $state)) {
       if($state -eq [states]::Jump) {
-         Write-Host -ForegroundColor Gray "Awaiting Crouch..."
+         Write-Host -ForegroundColor DarkGray "Awaiting Crouch..."
       }
 
-      $key = $Host.UI.RawUI.ReadKey()
+      if($state -eq [states]::Ready) {
+         $cumlative = $cumlative + $chance
 
-      if ($key -eq $duckkey) {
-         if($state -eq [states]::Ready) {
-            # Crouched First.
-            Write-Host -ForegroundColor Yellow " Key Pressed (Crouch)"
-            $startTime = Get-Date
-            $state = [states]::Crouch
-         } elseif(($state -eq [states]::Jump) -or ($state -eq [states]::JumpWarned)) {
-            # Happy Path
-            Write-Host -ForegroundColor Green " Key Pressed (Crouch)"
-            
-            $calculated = Get-Date - $startTime
-            $elapsedFrames = $calculated.TotalSeconds / $frameTime
-            $differenceSeconds = $frameTime - $calculated.TotalSeconds
-
-            if($elapsedFrames -lt 1) {
-               $chance = $elapsedFrames * 100
-               $message = "Crouch slightly *later* by {0:n5} seconds" -f $differenceSeconds + " to improve."
-            }  
-            elseif ($elapsedFrames -lt 2) {
-               $chance = ( (2 - $elapsedFrames) ) * 100
-               $message = "Crouch slightly *sooner* by {0:n5} seconds" -f ($differenceSeconds * -1) + " to improve."
-            } 
-            else {
-               $message = "Crouched too late by {0:n5} seconds" -f ($differenceSeconds * -1)
-               $chance = 0
-            }
-
-            ("{0:n1} frames have passed." -f $elapsedFrames.ToString()) | Write-Host
-            
-            ("{0:n1} % chance to hit." -f $chance.ToString()) | Write-Host
-            
-            
-            if($chance -gt 0) {
-               $message | Write-Host -ForegroundColor Green
-            } else {
-               $message | Write-Host -ForegroundColor Yellow
-            }
-
-            ReadyUp
-         } elseif ($state -eq [states]::Crouch) {
-            # Double Crouch
-            Write-Host -ForegroundColor Yellow " Key Pressed (Crouch)"
-            Write-Host -ForegroundColor Red " Double Crouch Input, Resetting"
-            $attempt = $attempt - 1
-            $chance = 0
-            ReadyUp
+         if (!($attempt -eq 0)) {
+            $average = $cumlative / $attempt
+            "###### Attempt {1} - Average: {0:n2}% ######" -f $attempt, $average
+            Write-Host -ForegroundColor DarkGray "Awaiting Jump..."
+         } else {
+            "###### Attempt 0 - Average: NA ######"
+            "Awaiting Jump..."
          }
-      } elseif($key -eq $jumpkey) {
-         if($state -eq [states]::Ready) {
-            # Happy Path
-            Write-Host -ForegroundColor Green " Key Pressed (Jump)"
-            $startTime = Get-Date
-            $state = [states]::Jump
-         } elseif($state -eq [states]::Jump) {
-            # Multi Jump Input.
-            Write-Host -ForegroundColor Gray " Key Pressed (Jump) - Ignored"
-            $state = [states]::JumpWarned
-            Write-Host -ForegroundColor Yellow "Warning: Multiple jumps detected, results may not reflect ingame behavior."
-         } elseif ($state -eq [states]::JumpWarned) {
-            # Multi Jump input, already warned.
-            Write-Host -ForegroundColor Gray " Key Pressed (Jump) - Ignored"
-            $state = [states]::JumpWarned
-         } elseif ($state -eq [states]::Crouch) {
-            Write-Host -ForegroundColor Yellow "0% chance to hit, jump must come 1 frame before crouch."
-            # Difference in time between inputs + 1 frameTime for the offset.
-            $delta = (Get-Date - $startTime).TotalSeconds + $frameTime
-            $earlyBy = $delta.TotalSeconds / $frameTime
 
-            $chance = 0
-
-            "Press crouch later by {0:n2} frames ({0:n2}s)" - f $earlyBy, delta
-            ReadyUp
-         }
-      } else {
-         Write-Host -ForegroundColor Gray " Key Pressed (and Ignored)"
+         $attempt = $attempt + 1      
       }
+   }
+   
+   $lastState = $state
+
+   # Get input
+   $key = $Host.UI.RawUI.ReadKey()
+
+   # State Transitions
+   if ($key -eq $duckkey) {
+      if($state -eq [states]::Ready) {
+         # Crouched First.
+         Write-Host -ForegroundColor Yellow " Key Pressed (Crouch)"
+         $startTime = Get-Date
+         $state = [states]::Crouch
+      } elseif(($state -eq [states]::Jump) -or ($state -eq [states]::JumpWarned)) {
+         # Happy Path
+         Write-Host -ForegroundColor Green " Key Pressed (Crouch)"
+         
+         $now = Get-Date
+         $calculated = $now - $startTime
+         $elapsedFrames = $calculated.TotalSeconds / $frameTime
+         $differenceSeconds = $frameTime - $calculated.TotalSeconds
+
+         if($elapsedFrames -lt 1) {
+            $chance = $elapsedFrames * 100
+            $message = "Crouch slightly *later* by {0:n5} seconds" -f $differenceSeconds + " to improve."
+         }  
+         elseif ($elapsedFrames -lt 2) {
+            $chance = ( (2 - $elapsedFrames) ) * 100
+            $message = "Crouch slightly *sooner* by {0:n5} seconds" -f ($differenceSeconds * -1) + " to improve."
+         } 
+         else {
+            $message = "Crouched too late by {0:n5} seconds" -f ($differenceSeconds * -1)
+            $chance = 0
+         }
+
+         ("{0:n1} frames have passed." -f $elapsedFrames.ToString()) | Write-Host
+         
+         
+         
+         
+         if($chance -gt 0) {
+            ("{0:n1}% chance to hit." -f $chance.ToString()) | Write-Host -ForegroundColor Green
+            $message | Write-Host -ForegroundColor Yellow
+         } else {
+            "0% chance to hit." | Write-Host -ForegroundColor Red
+            $message | Write-Host -ForegroundColor Yellow
+         }
+
+         $state = [states]::Ready
+      } elseif ($state -eq [states]::Crouch) {
+         # Double Crouch
+         Write-Host -ForegroundColor Yellow " Key Pressed (Crouch)"
+         Write-Host -ForegroundColor Red " Double Crouch Input, Resetting"
+         $attempt = $attempt - 1
+         $chance = 0
+         $state = [states]::Ready
+      }
+   } elseif($key -eq $jumpkey) {
+      if($state -eq [states]::Ready) {
+         # Happy Path
+         Write-Host -ForegroundColor Green " Key Pressed (Jump)"
+         $startTime = Get-Date
+         $state = [states]::Jump
+      } elseif($state -eq [states]::Jump) {
+         # Multi Jump Input.
+         Write-Host -ForegroundColor DarkGray " Key Pressed (Jump) - Ignored"
+         $state = [states]::JumpWarned
+         Write-Host -ForegroundColor Yellow "Warning: Multiple jumps detected, results may not reflect ingame behavior."
+      } elseif ($state -eq [states]::JumpWarned) {
+         # Multi Jump input, already warned.
+         Write-Host -ForegroundColor DarkGray " Key Pressed (Jump) - Ignored"
+         $state = [states]::JumpWarned
+      } elseif ($state -eq [states]::Crouch) {
+         Write-Host -ForegroundColor Red "0% chance to hit, wrong input order"
+         # Difference in time between inputs + 1 frameTime for the offset.
+         $now = Get-Date
+         $delta = ($now - $startTime).TotalSeconds + $frameTime
+         $earlyBy = $delta.TotalSeconds / $frameTime
+
+         $chance = 0
+
+         ("Press crouch later by {0:n2} frames ({0:n2}s)" -f $earlyBy, $delta) | Write-Host -ForegroundColor Yellow
+         $state = [states]::Ready
+      }
+   } else {
+      Write-Host -ForegroundColor DarkGray " Key Pressed (and Ignored)"
    }
 
    
 	# Start-Sleep -Milliseconds 125
-	# $HOST.UI.RawUI.Flushinputbuffer()
+	$HOST.UI.RawUI.Flushinputbuffer()
 }
